4327af1090d7b60744112d4b97612f3823b06678
smasiu <szymon.masko32@gmail.com>
2020-06-09 21:09:47 +0200

    hamburger menu animation

--- src/components/atoms/hamburger-menu/hamburger-menu.style.ts
+++ src/components/atoms/hamburger-menu/hamburger-menu.style.ts
@@ -1,7 +1,41 @@
 import styled, { css } from "styled-components";
 import { ThemeProps } from 'types/theme.types'
 
-export const HamburgerMenuWrapper = styled.div`
+type HamburgerMenuWrapperProps = {
+    open: boolean
+} & ThemeProps
+
+export const HamburgerItem = styled.div`
+    height: 2px; 
+    border-radius: 2px;
+    width: 100%;
+    transition: transform .2s;
+    will-change: transform;
+
+    ${({ theme }: ThemeProps) => css`
+        background-color: ${theme.colors.dark300};
+    `}
+`
+
+export const HamburgerItemWrapper = styled.div`
+    transition: transform .2s;
+    will-change: transform;
+`
+
+export const HamburgerItemWrapperTop = styled(HamburgerItemWrapper)`
+    width: 100%;
+`
+
+export const HamburgerItemWrapperCenter = styled(HamburgerItemWrapper)`
+    width: 70%;
+    transform-origin: right;
+`
+
+export const HamburgerItemWrapperBottom = styled(HamburgerItemWrapper)`
+    width: 90%;
+`
+
+export const HamburgerMenuWrapper = styled.div<HamburgerMenuWrapperProps>`
     width: 30px;
     height: 23px;
     display: flex;
@@ -9,25 +43,28 @@ export const HamburgerMenuWrapper = styled.div`
     justify-content: space-between;
     align-items: flex-end;
     cursor: pointer;
-`
+    
+    ${({open}: HamburgerMenuWrapperProps) => open && css`
+        ${HamburgerItemWrapperCenter} {
+            transform: scaleX(0);
+            transition: all .1s;
+        }
 
-export const HamburgerItem = styled.div`
-    height: 2px; 
-    border-radius: 2px;
-    width: 100%;
-    ${({ theme }: ThemeProps) => css`
-        background-color: ${theme.colors.dark300};
+        ${HamburgerItemWrapperTop} {
+            transform: translateY(10px);
+
+            ${HamburgerItem} {
+                transform: rotate(45deg);
+            }
+        }
+
+        ${HamburgerItemWrapperBottom} {
+            transform: translateY(-11px);
+            width: 100%;
+
+            ${HamburgerItem} {
+                transform: rotate(-45deg);
+            }
+        }
     `}
-`
-
-export const HamburgerItemTop = styled(HamburgerItem)`
-    width: 100%;
-`
-
-export const HamburgerItemCenter = styled(HamburgerItem)`
-    width: 70%;
-`
-
-export const HamburgerItemBottom = styled(HamburgerItem)`
-    width: 90%;
 `
--- src/components/atoms/hamburger-menu/hamburger-menu.tsx
+++ src/components/atoms/hamburger-menu/hamburger-menu.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { HamburgerMenuWrapper, HamburgerItemTop, HamburgerItemCenter, HamburgerItemBottom } from './hamburger-menu.style'
+import React, { useState } from 'react'
+import {
+    HamburgerMenuWrapper,
+    HamburgerItemWrapperTop,
+    HamburgerItemWrapperCenter,
+    HamburgerItemWrapperBottom,
+    HamburgerItem
+} from './hamburger-menu.style'
 
-export const HamburgerMenu = () => (
-    <HamburgerMenuWrapper>
-        <HamburgerItemTop />
-        <HamburgerItemCenter />
-        <HamburgerItemBottom />
-    </HamburgerMenuWrapper>
-)
+export const HamburgerMenu = () => {
+
+    const [open, setOpen] = useState(false)
+
+    const handleClick = () => setOpen(!open)
+
+    return (
+        <HamburgerMenuWrapper onClick={handleClick} open={open}>
+            <HamburgerItemWrapperTop>
+                <HamburgerItem />
+            </HamburgerItemWrapperTop>
+            <HamburgerItemWrapperCenter>
+                <HamburgerItem />
+            </HamburgerItemWrapperCenter>
+            <HamburgerItemWrapperBottom>
+                <HamburgerItem />
+            </HamburgerItemWrapperBottom>
+        </HamburgerMenuWrapper>
+    )
+}
