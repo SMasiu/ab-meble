11efdd8320674da0d8ec2364bfa1c1bdead97996
smasiu <szymon.masko.dev@gmail.com>
2020-06-09 10:25:15 +0200

    welcome home page

---
+++ src/components/organisms/landing-welcome/landing-welcome.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LandingWelcome = () => (
+	<section>
+		<img src={'/welcome.jpg'} alt="welcome" />
+	</section>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import Layout from 'components/templates/layout/layout'
 import SEO from 'components/templates/seo/seo'
+import { LandingWelcome } from 'components/organisms/landing-welcome/landing-welcome'
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" />
+		<LandingWelcome />
 	</Layout>
 )
 
