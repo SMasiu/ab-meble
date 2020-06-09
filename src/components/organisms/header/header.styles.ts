b5af45895c57a63644c8a7e6ffb21d2b81c8f8ea
smasiu <szymon.masko.dev@gmail.com>
2020-06-09 10:28:04 +0200

    fixed logo

--- src/components/organisms/header/header.styles.ts
+++ src/components/organisms/header/header.styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
 export const HeaderWrapper = styled.header`
+	position: fixed;
+	top: 0;
+	width: 100vw;
 	min-height: 75px;
 	padding: 5px 20px;
 	display: flex;
