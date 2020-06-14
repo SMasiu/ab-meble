aa525769c3d101b60d4e1de880b5b1f3900b8c05
smasiu <szymon.masko.dev@gmail.com>
2020-06-14 23:17:04 +0200

    desktop projects

--- src/components/organisms/landing-projects/landing-projects.styles.ts
+++ src/components/organisms/landing-projects/landing-projects.styles.ts
@@ -1,12 +1,24 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { ThemeProps } from 'types/theme.types'
+
+type ImageProps = {
+	path: string
+} & ThemeProps
 
 export const LandingProjectsOuterWrapper = styled.section`
-	min-height: 1600px;
+	min-height: 1800px;
 	height: 2vh;
+	position: relative;
 `
 
 export const LandingProjectsWrapper = styled.div`
+	position: relative;
+	height: 100%;
+	width: 1800px;
+	margin: auto;
+	padding: 100px 0;
 	display: grid;
+	row-gap: 200px;
 	grid-template-columns: 2fr 1fr 1fr;
 	grid-template-rows: repeat(3, minmax(400px, 1fr));
 	grid-template-areas:
@@ -15,7 +27,19 @@ export const LandingProjectsWrapper = styled.div`
 		'image-name-wrapper-3 image-wrapper-2 article-wrapper-2';
 `
 
-export const ImageNameWrapper = styled.section``
+export const ImageNameWrapper = styled.section`
+	display: grid;
+	grid-template-columns: 250px 1fr;
+	gap: 50px;
+	align-items: center;
+	justify-items: center;
+	position: relative;
+`
+
+export const NameHeading = styled.h2`
+	font-size: 6rem;
+	font-family: 'Comfortaa', cursive;
+`
 
 export const ImageNameWrapper1 = styled(ImageNameWrapper)`
 	grid-area: image-name-wrapper-1;
@@ -29,7 +53,12 @@ export const ImageNameWrapper3 = styled(ImageNameWrapper)`
 	grid-area: image-name-wrapper-3;
 `
 
-export const ImageWrapper = styled.section``
+export const ImageWrapper = styled.section`
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`
 
 export const ImageWrapper1 = styled(ImageWrapper)`
 	grid-area: image-wrapper-1;
@@ -39,7 +68,11 @@ export const ImageWrapper2 = styled(ImageWrapper)`
 	grid-area: image-wrapper-2;
 `
 
-export const ArticleWrapper = styled.article``
+export const ArticleWrapper = styled.article`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
 
 export const ArticleWrapper1 = styled(ArticleWrapper)`
 	grid-area: article-wrapper-1;
@@ -47,4 +80,61 @@ export const ArticleWrapper1 = styled(ArticleWrapper)`
 
 export const ArticleWrapper2 = styled(ArticleWrapper)`
 	grid-area: article-wrapper-2;
+`
+
+export const Image = styled.figure<ImageProps>`
+	width: 400px;
+	height: 400px;
+	background-position: center;
+	background-size: cover;
+	border-radius: 50%;
+
+	${({ path, theme }: ImageProps) => css`
+		background-image: url(${path});
+		border: solid 4px ${theme.colors.light300};
+	`}
+`
+
+export const ImageContentWrapper = styled.div`
+	position: absolute;
+`
+
+export const ImageContentWrapper1 = styled(ImageContentWrapper)`
+	top: 200px;
+`
+
+export const ImageContentWrapper2 = styled(ImageContentWrapper)`
+	top: -200px;
+`
+
+export const ProjectsBackground = styled.section`
+	position: absolute;
+	top: calc(50% - 300px);
+	width: 100%;
+	height: 600px;
+	background-image: url('projects-cover.jpg');
+	background-size: cover;
+	background-position: center;
+	background-attachment: fixed;
+`
+
+export const LineThrough = styled.div`
+	width: 4px;
+	transform: translateX(-50%);
+	position: absolute;
+	${({ theme }: ThemeProps) => css`
+		background-color: ${theme.colors.light300};
+	`};
+`
+
+export const LineThroughLeft = styled(LineThrough)`
+	height: 100%;
+	left: 600px;
+`
+
+export const LineThroughRight = styled(LineThrough)`
+	top: 50%;
+	height: 50%;
+	transform: translate(-50%, -50%);
+	right: 670px;
 `
--- src/components/organisms/landing-projects/landing-projects.tsx
+++ src/components/organisms/landing-projects/landing-projects.tsx
@@ -9,18 +9,62 @@ import {
 	ImageWrapper2,
 	ArticleWrapper1,
 	ArticleWrapper2,
+	NameHeading,
+	Image,
+	ImageContentWrapper1,
+	ImageContentWrapper2,
+	ProjectsBackground,
+	LineThroughLeft,
+	LineThroughRight,
 } from './landing-projects.styles'
+import { WelcomeInfo } from 'components/molecules/welcome-info/welcome-info'
 
 export const LandingProjects = () => (
 	<LandingProjectsOuterWrapper>
+		<ProjectsBackground />
 		<LandingProjectsWrapper>
-			<ImageNameWrapper1>1</ImageNameWrapper1>
-			<ImageNameWrapper2>2</ImageNameWrapper2>
-			<ImageNameWrapper3>3</ImageNameWrapper3>
-			<ImageWrapper1>4</ImageWrapper1>
-			<ImageWrapper2>5</ImageWrapper2>
-			<ArticleWrapper1>6</ArticleWrapper1>
-			<ArticleWrapper2>7</ArticleWrapper2>
+			<LineThroughLeft />
+			<LineThroughRight />
+			<ImageNameWrapper1>
+				<NameHeading>Biura</NameHeading>
+				<Image path="project3.jpg" />
+			</ImageNameWrapper1>
+			<ImageNameWrapper2>
+				<NameHeading>Domy</NameHeading>
+				<Image path="project4.jpg" />
+			</ImageNameWrapper2>
+			<ImageNameWrapper3>
+				<NameHeading>Salony</NameHeading>
+				<Image path="project5.jpg" />
+			</ImageNameWrapper3>
+			<ImageWrapper1>
+				<ImageContentWrapper1>
+					<Image path="project1.jpg" />
+				</ImageContentWrapper1>
+			</ImageWrapper1>
+			<ImageWrapper2>
+				<ImageContentWrapper2>
+					<Image path="project2.jpg" />
+				</ImageContentWrapper2>
+			</ImageWrapper2>
+			<ArticleWrapper1>
+				<WelcomeInfo
+					heading={'Lorem, ipsum dolor.'}
+					paragraph={
+						'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, repudiandae?'
+					}
+					discover={'Discover'}
+				></WelcomeInfo>
+			</ArticleWrapper1>
+			<ArticleWrapper2>
+				<WelcomeInfo
+					heading={'Lorem, ipsum dolor.'}
+					paragraph={
+						'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, repudiandae?'
+					}
+					discover={'Discover'}
+				></WelcomeInfo>
+			</ArticleWrapper2>
 		</LandingProjectsWrapper>
 	</LandingProjectsOuterWrapper>
 )
