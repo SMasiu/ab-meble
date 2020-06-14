40243f4cd68e3e36ced34c98ac862c78849d5e94
smasiu <szymon.masko.dev@gmail.com>
2020-06-14 12:26:59 +0200

    Updates

---
+++ component---src-pages-404-tsx-4b4989715d244e465f13.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),l=n("tDBm"),c=n("uZ4q");t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(c.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-4b4989715d244e465f13.js.map
