f1c39c3c35b1f95b7f2c3e89b8665d7e9879b51a
smasiu <szymon.masko.dev@gmail.com>
2020-06-14 20:22:35 +0200

    projects

--- src/components/organisms/landing-projects/landing-projects.styles.ts
+++ src/components/organisms/landing-projects/landing-projects.styles.ts
@@ -1,215 +1,50 @@
-import styled, { css } from 'styled-components'
-import { ThemeProps } from 'types/theme.types'
-import { LineThrough } from 'components/atoms/line-through/line-through'
+import styled from 'styled-components'
 
-type ImageWrapperProps = {
-	url: string
-} & ThemeProps
-
-export const ItemsSectionWrapper = styled.section`
-	height: 100%;
-	display: grid;
-	gap: 25px;
-`
-
-export const ItemsSectionWrapperLeft = styled(ItemsSectionWrapper)`
-	grid-template-columns: 1fr 400px;
-	z-index: 3000;
-`
-
-export const LeftLineThrough = styled(LineThrough)``
-
-export const ItemsSectionWrapperRight = styled(ItemsSectionWrapper)`
-	grid-template-columns: 400px 1fr;
-`
-
-export const ContentWrapper = styled.div`
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: space-evenly;
-`
-export const ContentWrapperLeftNames = styled(ContentWrapper)``
-export const ContentWrapperRightImages = styled(ContentWrapper)``
-export const ContentWrapperRightDesc = styled(ContentWrapper)``
-
-export const ImageWrapper = styled.figure<ImageWrapperProps>`
-	position: relative;
-	width: 400px;
-	height: 400px;
-	border-radius: 50%;
-	background-size: cover;
-
-	${({ theme }: ThemeProps) => css`
-		border: solid 4px ${theme.colors.light300};
-	`}
-
-	${({ url }: ImageWrapperProps) => css`
-		background-image: url(${url});
-	`}
-`
-
-export const ArticleWrapper = styled.article`
-	height: 400px;
-	display: flex;
-	align-items: center;
-`
-
-export const ArticleHeading = styled.h1`
-	font-size: 6rem;
-	font-family: 'Comfortaa', cursive;
-`
-
-export const ProjectsBackgroundWrapper = styled.div`
-	z-index: 4000;
-	position: absolute;
-	top: calc(50% - 300px);
-	width: 100%;
-	height: 600px;
-`
-
-export const ProjectsBackground = styled.section`
-	height: 100%;
-	background-attachment: fixed;
-	background-image: url('projects-cover.jpg');
-	background-size: cover;
-	background-position: center;
-	background-repeat: no-repeat;
-`
-
-export const HalfLineThrough = styled(LineThrough)`
-	height: 50%;
-`
-
-export const LandingProjectsWrapper = styled.section`
-	z-index: 5000;
-	position: relative;
-	width: 1800px;
-	height: 100%;
-	margin: auto;
-	display: grid;
-	grid-template-columns: 800px 1fr;
-	gap: 50px;
-
-	@media screen and (max-width: 1850px) {
-		width: 1400px;
-		grid-template-columns: 600px 1fr;
-	}
-
-	@media screen and (max-width: 1450px) {
-		width: 90%;
-		grid-template-columns: 3fr 1fr;
-		gap: 0;
-		${ContentWrapperRightImages} {
-			display: none;
-		}
-
-		${ItemsSectionWrapperLeft} {
-			grid-template-columns: 1fr 2fr;
-			gap: 0;
-		}
-
-		${ItemsSectionWrapperRight} {
-			grid-template-columns: 1fr;
-			gap: 0;
-		}
-	}
-
-	@media screen and (max-width: 1000px) {
-		grid-template-columns: 1fr;
-		position: relative;
-
-		${ItemsSectionWrapperLeft} {
-			grid-template-columns: 1fr;
-			position: relative;
-		}
-
-		${ContentWrapperLeftNames} {
-			position: absolute;
-			height: 100%;
-			top: 0;
-			left: 50%;
-			z-index: 4000;
-			transform: translateX(-50%);
-			width: 400px;
-
-			${ArticleWrapper} {
-				width: 100%;
-			}
-
-			${ArticleHeading} {
-				width: 100%;
-				border-radius: 5px;
-				text-align: center;
-				padding: 10px;
-				${({ theme }: ThemeProps) => css`
-					background-color: ${theme.colors.light100};
-				`};
-			}
-		}
-
-		${ContentWrapperRightDesc} {
-			position: relative;
-			z-index: 6000;
-		}
-
-		${ItemsSectionWrapperRight} {
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			height: 60%;
-			transform: translate(-50%, -50%);
-			width: 350px;
-			${({ theme }: ThemeProps) => css`
-				background-color: ${theme.colors.light100 + 'bb'};
-			`}
-
-			${ArticleWrapper} {
-				z-index: 6000;
-				height: auto;
-				padding: 0 25px;
-			}
-		}
-
-		${LeftLineThrough} {
-			display: none;
-		}
-	}
-
-	@media screen and (max-width: 500px) {
-		width: 95%;
-		${ItemsSectionWrapperRight} {
-			width: 275px;
-		}
-
-		${ItemsSectionWrapperLeft} ${ArticleWrapper} {
-			height: 300px;
-		}
-
-		${ContentWrapperLeftNames} {
-			width: 300px;
-		}
-
-		${ImageWrapper} {
-			width: 300px;
-			height: 300px;
-		}
-	}
-`
-
-export const LandingProjectsOuterWrapper = styled.div`
-	height: 200vh;
+export const LandingProjectsOuterWrapper = styled.section`
 	min-height: 1600px;
-	position: relative;
+	height: 2vh;
+`
 
-	@media screen and (max-width: 1000px) {
-		height: 2000px;
-		min-height: 2000px;
-	}
+export const LandingProjectsWrapper = styled.div`
+	display: grid;
+	grid-template-columns: 2fr 1fr 1fr;
+	grid-template-rows: repeat(3, minmax(400px, 1fr));
+	grid-template-areas:
+		'image-name-wrapper-1 image-wrapper-1 article-wrapper-1'
+		'image-name-wrapper-2 . .'
+		'image-name-wrapper-3 image-wrapper-2 article-wrapper-2';
+`
 
-	@media screen and (max-width: 500px) {
-		height: 1650px;
-		min-height: 1650px;
-	}
+export const ImageNameWrapper = styled.section``
+
+export const ImageNameWrapper1 = styled(ImageNameWrapper)`
+	grid-area: image-name-wrapper-1;
+`
+
+export const ImageNameWrapper2 = styled(ImageNameWrapper)`
+	grid-area: image-name-wrapper-2;
+`
+
+export const ImageNameWrapper3 = styled(ImageNameWrapper)`
+	grid-area: image-name-wrapper-3;
+`
+
+export const ImageWrapper = styled.section``
+
+export const ImageWrapper1 = styled(ImageWrapper)`
+	grid-area: image-wrapper-1;
+`
+
+export const ImageWrapper2 = styled(ImageWrapper)`
+	grid-area: image-wrapper-2;
+`
+
+export const ArticleWrapper = styled.article``
+
+export const ArticleWrapper1 = styled(ArticleWrapper)`
+	grid-area: article-wrapper-1;
+`
+
+export const ArticleWrapper2 = styled(ArticleWrapper)`
+	grid-area: article-wrapper-2;
 `
--- src/components/organisms/landing-projects/landing-projects.tsx
+++ src/components/organisms/landing-projects/landing-projects.tsx
@@ -1,79 +1,26 @@
 import React from 'react'
 import {
-	LandingProjectsWrapper,
-	ItemsSectionWrapperLeft,
-	ItemsSectionWrapperRight,
-	ContentWrapper,
-	ImageWrapper,
-	ArticleWrapper,
-	ArticleHeading,
 	LandingProjectsOuterWrapper,
-	ProjectsBackground,
-	HalfLineThrough,
-	ProjectsBackgroundWrapper,
-	ContentWrapperRightImages,
-	ContentWrapperLeftNames,
-	ContentWrapperRightDesc,
-	LeftLineThrough,
+	LandingProjectsWrapper,
+	ImageNameWrapper1,
+	ImageNameWrapper2,
+	ImageNameWrapper3,
+	ImageWrapper1,
+	ImageWrapper2,
+	ArticleWrapper1,
+	ArticleWrapper2,
 } from './landing-projects.styles'
-import { ThemeColorNames } from 'types/theme.types'
-import { WelcomeInfo } from 'components/molecules/welcome-info/welcome-info'
 
 export const LandingProjects = () => (
 	<LandingProjectsOuterWrapper>
-		<ProjectsBackgroundWrapper>
-			<ProjectsBackground />
-		</ProjectsBackgroundWrapper>
 		<LandingProjectsWrapper>
-			<ItemsSectionWrapperLeft>
-				<ContentWrapperLeftNames>
-					<ArticleWrapper>
-						<ArticleHeading>Biura</ArticleHeading>
-					</ArticleWrapper>
-					<ArticleWrapper>
-						<ArticleHeading>Domy</ArticleHeading>
-					</ArticleWrapper>
-					<ArticleWrapper>
-						<ArticleHeading>Salony</ArticleHeading>
-					</ArticleWrapper>
-				</ContentWrapperLeftNames>
-				<ContentWrapper>
-					<LeftLineThrough color={ThemeColorNames.light300} />
-					<ImageWrapper url={'project3.jpg'}></ImageWrapper>
-					<ImageWrapper url={'project4.jpg'}></ImageWrapper>
-					<ImageWrapper url={'project5.jpg'}></ImageWrapper>
-				</ContentWrapper>
-			</ItemsSectionWrapperLeft>
-			<ItemsSectionWrapperRight>
-				<ContentWrapperRightImages>
-					<HalfLineThrough color={ThemeColorNames.light300} />
-					<ImageWrapper url={'project1.jpg'}></ImageWrapper>
-					<ImageWrapper url={'project2.jpg'}></ImageWrapper>
-				</ContentWrapperRightImages>
-				<ContentWrapperRightDesc>
-					<ArticleWrapper>
-						<WelcomeInfo
-							heading={'Lorem, ipsum dolor.'}
-							paragraph={
-								'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, repudiandae?'
-							}
-							discover={'Discover'}
-						></WelcomeInfo>
-					</ArticleWrapper>
-					<ArticleWrapper>
-						<div></div>
-					</ArticleWrapper>
-					<ArticleWrapper>
-						<WelcomeInfo
-							heading={'Lorem, ipsum dolor.'}
-							paragraph={
-								'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, repudiandae?'
-							}
-							discover={'Discover'}
-						></WelcomeInfo>
-					</ArticleWrapper>
-				</ContentWrapperRightDesc>
-			</ItemsSectionWrapperRight>
+			<ImageNameWrapper1>1</ImageNameWrapper1>
+			<ImageNameWrapper2>2</ImageNameWrapper2>
+			<ImageNameWrapper3>3</ImageNameWrapper3>
+			<ImageWrapper1>4</ImageWrapper1>
+			<ImageWrapper2>5</ImageWrapper2>
+			<ArticleWrapper1>6</ArticleWrapper1>
+			<ArticleWrapper2>7</ArticleWrapper2>
 		</LandingProjectsWrapper>
 	</LandingProjectsOuterWrapper>
 )
